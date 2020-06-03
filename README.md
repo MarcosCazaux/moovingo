4f8cb7b5bf2c376c2153bfa29e9fe9a7ebddd414
Marcos Cazaux <info@marcoscazaux.com>
2020-06-03 20:52:26 +0200

    SASS and Tailwindcss implementation

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# moovingo
+# MOOVINGO Test Project
+
+```
+by Marcos Cazaux for SQUARE 1
+```
+
+## Technologies Used
+
+```
+VueJS
+Tailwindcss
+SASS
+```
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Lints and fixes files
+
+```
+npm run lint
+```
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        neutral: "var(--color-neutral)",
+        graylight: "var(--color-graylight)",
+        graydark: "var(--color-graydark)",
+        green: "var(--color-green)",
+        greenlight: "var(--color-greenlight)",
+        orange: "var(--color-orange)",
+        blue: "var(--color-blue)"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
